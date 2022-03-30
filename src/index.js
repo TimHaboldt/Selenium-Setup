e13598297747fff2a2589cd9140dcf3fc420dd3a
Haboldt <tim.haboldt@campusjaeger.de>
2022-03-30 12:29:17 +0200

    [F] Showcase assert

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {Builder, By, Key, until} from 'selenium-webdriver'
+import assert from "assert";
 
 function delay (ms) {
     return new Promise( resolve => setTimeout(resolve, ms) );
@@ -7,11 +8,14 @@ function delay (ms) {
 (async function example() {
     // Only works if you have chrome installed. Otherwise use firefox
     let driver = await new Builder().forBrowser('chrome').build();
+    await driver.manage().setTimeouts({implicit: 300000})
+    await driver.manage().window().maximize();
+
     try {
         await driver.get('http://www.workwise.io');
-        await driver.manage().setTimeouts({implicit: 300000})
         await driver.findElement(By.xpath("//span[contains(text(),'Alle akzeptieren')]")).click();
-        await delay(10000);
+        const loginButtonContent = await driver.findElement(By.xpath("//button")).getText();
+        assert.equal(loginButtonContent, 'Einloggen');
     } finally {
         await driver.quit();
     }
